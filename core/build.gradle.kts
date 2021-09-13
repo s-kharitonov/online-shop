08fdaea58b83f0f9302061e986fd37baf5f5bcfc
skharitonov <skharitonov.k@gmail.com>
2021-09-13 22:02:36 +0300

    added core module

---
+++ core/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
+}
+
+dependencies {
+    implementation("org.postgresql:postgresql")
+    implementation("org.liquibase:liquibase-core")
+
+    implementation("org.apache.logging.log4j:log4j-api")
+    implementation("org.apache.logging.log4j:log4j-core")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl")
+    implementation(libs.liquibase.slf4j)
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation(libs.springfox.boot.starter)
+    implementation(libs.springfox.bean.validators)
+    implementation(libs.springfox.data.rest)
+    implementation(libs.springfox.swagger.ui)
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(libs.testcontainers.junit.jupiter)
+    testImplementation(libs.testcontainers.postgresql)
+}
+
+tasks.getByName<BootJar>("bootJar") {
+    mainClass.set("com.core.CoreApp")
+    archiveBaseName.set("core")
+    archiveVersion.set("0.1.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,51 @@
+enableFeaturePreview("VERSION_CATALOGS")
+
+pluginManagement {
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+
+    plugins {
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springDependencyManagementVersion
+    }
+}
+
+dependencyResolutionManagement {
+    val testContainersVersion: String by settings
+    val liquibaseSlf4jVersion: String by settings
+    val springFoxVersion: String by settings
+
+    versionCatalogs {
+        create("libs") {
+            version("testcontainers", testContainersVersion)
+            version("liquibaseSlf4j", liquibaseSlf4jVersion)
+            version("springFox", springFoxVersion)
+
+            alias("testcontainers-junit-jupiter")
+                .to("org.testcontainers", "junit-jupiter")
+                .versionRef("testcontainers")
+            alias("testcontainers-postgresql")
+                .to("org.testcontainers", "postgresql")
+                .versionRef("testcontainers")
+            alias("liquibase-slf4j")
+                .to("com.mattbertolini", "liquibase-slf4j")
+                .versionRef("liquibaseSlf4j")
+            alias("springfox-boot-starter")
+                .to("io.springfox", "springfox-boot-starter")
+                .versionRef("springFox")
+            alias("springfox-bean-validators")
+                .to("io.springfox", "springfox-bean-validators")
+                .versionRef("springFox")
+            alias("springfox-data-rest")
+                .to("io.springfox", "springfox-data-rest")
+                .versionRef("springFox")
+            alias("springfox-swagger-ui")
+                .to("io.springfox", "springfox-swagger-ui")
+                .versionRef("springFox")
+        }
+    }
+}
+
 rootProject.name = "online-shop"
+
+include("core")
