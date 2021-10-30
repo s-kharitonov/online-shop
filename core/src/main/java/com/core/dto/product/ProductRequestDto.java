5a70c90e19b861847810ab15e46d486844e63fc9
skharitonov <skharitonov.k@gmail.com>
2021-10-30 14:38:25 +0300

    change annotation

--- core/src/main/java/com/core/dto/product/ProductRequestDto.java
+++ core/src/main/java/com/core/dto/product/ProductRequestDto.java
@@ -1,14 +1,13 @@
 package com.core.dto.product;
 
-import org.springframework.lang.NonNull;
-
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
-public record ProductRequestDto(@NonNull @Min(0) BigDecimal price,
+public record ProductRequestDto(@NotNull @Min(0) BigDecimal price,
                                 @Valid @NotEmpty List<ProductDescriptionRequestDto> descriptions,
                                 @Valid @NotEmpty List<ProductFeatureRequestDto> features) {
 }
