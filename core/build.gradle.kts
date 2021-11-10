6e3349f2c4ed06a204667a3c0dffced85a2cc064
skharitonov <skharitonov.k@gmail.com>
2021-11-10 21:44:12 +0300

    migration to java 17

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_16
-    targetCompatibility = JavaVersion.VERSION_16
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
