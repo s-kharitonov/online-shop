dfef37129364dd798ee15fe73e9505e102f14c35
skharitonov <skharitonov.k@gmail.com>
2022-01-11 21:42:30 +0300

    migration from springfox to springdoc openapi

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,23 +3,27 @@ enableFeaturePreview("VERSION_CATALOGS")
 pluginManagement {
     val springBootVersion: String by settings
     val springDependencyManagementVersion: String by settings
+    val springDocPlugin: String by settings
+    val engelmanProcesses: String by settings
 
     plugins {
         id("org.springframework.boot") version springBootVersion
         id("io.spring.dependency-management") version springDependencyManagementVersion
+        id("org.springdoc.openapi-gradle-plugin") version springDocPlugin
+        id("com.github.johnrengelman.processes") version engelmanProcesses
     }
 }
 
 dependencyResolutionManagement {
     val testContainersVersion: String by settings
     val liquibaseSlf4jVersion: String by settings
-    val springFoxVersion: String by settings
+    val springDoc: String by settings
 
     versionCatalogs {
         create("libs") {
             version("testcontainers", testContainersVersion)
             version("liquibaseSlf4j", liquibaseSlf4jVersion)
-            version("springFox", springFoxVersion)
+            version("springDoc", springDoc)
 
             alias("testcontainers-junit-jupiter")
                 .to("org.testcontainers", "junit-jupiter")
@@ -30,18 +34,9 @@ dependencyResolutionManagement {
             alias("liquibase-slf4j")
                 .to("com.mattbertolini", "liquibase-slf4j")
                 .versionRef("liquibaseSlf4j")
-            alias("springfox-boot-starter")
-                .to("io.springfox", "springfox-boot-starter")
-                .versionRef("springFox")
-            alias("springfox-bean-validators")
-                .to("io.springfox", "springfox-bean-validators")
-                .versionRef("springFox")
-            alias("springfox-data-rest")
-                .to("io.springfox", "springfox-data-rest")
-                .versionRef("springFox")
-            alias("springfox-swagger-ui")
-                .to("io.springfox", "springfox-swagger-ui")
-                .versionRef("springFox")
+            alias("springdoc-openapi")
+                .to("org.springdoc", "springdoc-openapi-ui")
+                .versionRef("springDoc")
         }
     }
 }
